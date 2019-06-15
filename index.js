e98c03cd47cd2d91513f7048c6b0517369b42282
mubin <mubin@kisanforum.org>
2019-06-15 20:57:13 +0530

    Node MongoDB Example 2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const dbOpr = require('./operations');
 
 const url = 'mongodb://localhost:27017/';
 const dbName = 'conFusion';
@@ -11,7 +12,10 @@ mongoClient.connect(url, (err, client) => {
     console.log('Connected correctly to server');
 
     const db = client.db(dbName);
-    const collection = db.collection('dishes');
+
+    /** First Method to connect with mongoDB and its operation  */
+
+    /* const collection = db.collection('dishes');
 
     collection.insertOne({ "name": "Fruit Custard", "description": "With two scoops of butterscotch..." }, (err, result) => {
         assert.equal(err, null);
@@ -30,6 +34,25 @@ mongoClient.connect(url, (err, client) => {
 
                 client.close();
             })
+        });
+    }); */
+
+    /** Second Method to connect with mongoDB and its operation  */
+
+    dbOpr.insertDocument(db, { name: 'Vadonut', description: 'Test' }, 'dishes', (result) => {
+        console.log('Insert Document:\n', result.ops);
+        dbOpr.findDocuments(db, 'dishes', (docs) => {
+            console.log('Found Documents:\n', docs);
+            dbOpr.updateDocument(db, { name: 'Vadonut' }, { description: ' updation of description' }, 'dishes', (result) => {
+                console.log('Updated document:\n', result.result);
+                dbOpr.findDocuments(db, 'dishes', (docs) => {
+                    console.log('Found Updated Documents:\n', docs);
+                    db.dropCollection('dishes', (result) => {
+                        console.log('Dropped Collection: ', result);
+                        client.close();
+                    });
+                });
+            });
         });
     });
 
